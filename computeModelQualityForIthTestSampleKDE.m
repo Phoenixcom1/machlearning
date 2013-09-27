0a82efdf352cfd81806ffc4c6e287ead4560061c
Sebastian Richter <sebastian.richter@student.kit.edu>
2013-09-27 18:18:37 -0400

    added KDE evaluation 2

--- computeModelQualityForIthTestSampleKDE.m
+++ computeModelQualityForIthTestSampleKDE.m
@@ -3,6 +3,9 @@ function [ p ] = computeModelQualityForIthTestSampleKDE( data_vector, bandwidth,
 % density estimation using the given data_vector with the specified bandwidth 
 % and returns the probability p of the specified value.
 
-disp('computeModelQualityForIthTestSampleKDE has not yet been implemented');
+pd = fitdist(data_vector,'kernel','kernel','normal','support','unbounded','width',bandwidth);
+pre_log_p = pdf(pd, value);
+p = log(pre_log_p);
+% disp(['Probability for value ' num2str(value) ' with bandwidth ' num2str(bandwidth) ': ' num2str(pre_log_p)]);
 
 end
